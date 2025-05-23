06463af01d1c4ec933151806b560e23cffdb54d8
Iliya Brook <iliyabrook1987@gmail.com>
2025-05-24 00:46:28 +0300

    e

--- vite.config.ts
+++ vite.config.ts
@@ -12,104 +12,96 @@ import {
 	extensionReloaderWebSocket,
   } from "vite-plugin-extension-reloader";
 
-const isDev = process.env.__DEV__ === 'true'
-const isProduction = !isDev
-
 const rootDir = resolve(__dirname)
 const srcDir = resolve(rootDir, 'src')
 const lib = resolve(srcDir, 'lib')
 const assetsDir = resolve(srcDir, 'assets')
 const publicJsDir = resolve(rootDir, 'public')
 
-
-export default defineConfig({
-	resolve: {
-		alias: {
-			'@src': srcDir,
-			'@assets': assetsDir,
-			'@lib': lib,
-			'@public': publicJsDir
-		}
-	},
-	plugins: [
-		react(),
-		makeManifest(manifestPath, {
-			isDev,
-			contentScriptCssKey: regenerateCacheInvalidationKey()
-		}),
-		viteStaticCopy({
-			targets: [
-				{
-					src: 'models/deepfake-detection.onnx',
-					dest: 'models'
-				}
-			]
-		}),
-		isDev && extensionReloaderBuildStep('dist/manifest.json'),
-		extensionReloaderWebSocket(),
-		extensionReloaderWatchExternal("src/**/*")
-	],
-	build: {
-		modulePreload: isDev,
-		minify: isProduction,
-		reportCompressedSize: isProduction,
-		lib: {
-			entry: resolve(srcDir, 'content', 'content.ts'),
-			name: 'content',
-			fileName: () => "src/content/content.js",
-			formats: ['es']
-		},
-		rollupOptions: {
-			input: {
-				popup: resolve(srcDir, 'popup', 'index.html'),
-				logs: resolve(srcDir, 'logs', 'index.html'),
-				background: resolve(srcDir, 'background', 'background.ts'),
-				content: resolve(srcDir, 'content', 'content.ts'),
-				onnxInit: resolve(srcDir, 'background', 'onnx-init.js')
-			},
-			output: {
-				entryFileNames: (chunkInfo) => {
-					if (chunkInfo.name === 'onnxInit') {
-						return 'src/background/onnx-init.js'
-					}
-					return 'src/[name]/[name].js'
-				},
-				chunkFileNames: isDev
-					? 'assets/js/[name].js'
-					: 'assets/js/[name].[hash].js',
-				assetFileNames: (assetInfo) => {
-										const fileName =
-						(assetInfo.names && assetInfo.names[0]) ||
-						(assetInfo.originalFileNames && assetInfo.originalFileNames[0]) ||
-						''
-					const parsedPath = path.parse(fileName)
-					const baseName = parsedPath.base
-					const assetFolder = parsedPath.dir.split('/').at(-1)
-					const name =
-						assetFolder + firstUpperCase(baseName.replace(parsedPath.ext, ''))
-					if (name === 'Contentstyle') {
-						return `assets/css/contentStyle${cacheInvalidationKey}.chunk.css`
-					}
-					return `assets/[ext]/${name}.chunk.[ext]`
-				}
+//@ts-ignore
+export default defineConfig(({ _command, mode, _isSsrBuild, _isPreview }) => {
+	const isDev = mode === 'development'
+	const isProduction = !isDev
+	return {
+		resolve: {
+			alias: {
+				'@src': srcDir,
+				'@assets': assetsDir,
+				'@lib': lib,
+				'@public': publicJsDir
 			}
 		},
-		outDir: 'dist',
-		emptyOutDir: true,
-		copyPublicDir: true
-	},
-	define: {
-		__IS_DEV__: JSON.stringify(isDev)
-	},
-	optimizeDeps: {
-		exclude: ['onnxruntime-web']
-	},
-	publicDir: 'public',
-	assetsInclude: ['**/*.onnx'],
-	server: {
-		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp'
+		plugins: [
+			react(),
+			makeManifest(manifestPath, {
+				isDev,
+				contentScriptCssKey: regenerateCacheInvalidationKey()
+			}),
+			viteStaticCopy({
+				targets: [
+					{
+						src: 'models/deepfake-detection.onnx',
+						dest: 'models'
+					}
+				]
+			}),
+			isDev && extensionReloaderBuildStep('dist/manifest.json'),
+			extensionReloaderWebSocket(),
+			extensionReloaderWatchExternal("src/**/*")
+		],
+		build: {
+			modulePreload: isDev,
+			minify: isProduction,
+			reportCompressedSize: isProduction,
+			rollupOptions: {
+				input: {
+					popup: resolve(srcDir, 'popup', 'index.html'),
+					logs: resolve(srcDir, 'logs', 'index.html'),
+					background: resolve(srcDir, 'background', 'background.ts'),
+					content: resolve(srcDir, 'content', 'content.ts'),
+					onnxInit: resolve(srcDir, 'background', 'onnx-init.js')
+				},
+				output: {
+					entryFileNames: (chunkInfo) => {
+						if (chunkInfo.name === 'onnxInit') {
+							return 'src/background/onnx-init.js'
+						}
+						return 'src/[name]/[name].js'
+					},
+					chunkFileNames: isDev
+						? 'assets/js/[name].js'
+						: 'assets/js/[name].[hash].js',
+					assetFileNames: (assetInfo) => {
+											const fileName =
+							(assetInfo.names && assetInfo.names[0]) ||
+							(assetInfo.originalFileNames && assetInfo.originalFileNames[0]) ||
+							''
+						const parsedPath = path.parse(fileName)
+						const baseName = parsedPath.base
+						const assetFolder = parsedPath.dir.split('/').at(-1)
+						const name =
+							assetFolder + firstUpperCase(baseName.replace(parsedPath.ext, ''))
+						if (name === 'Contentstyle') {
+							return `assets/css/contentStyle${cacheInvalidationKey}.chunk.css`
+						}
+						return `assets/[ext]/${name}.chunk.[ext]`
+					}
+				}
+			},
+			outDir: 'dist',
+			emptyOutDir: true,
+			copyPublicDir: true
+		},
+		optimizeDeps: {
+			exclude: ['onnxruntime-web']
+		},
+		publicDir: 'public',
+		assetsInclude: ['**/*.onnx'],
+		server: {
+			headers: {
+				'Cross-Origin-Opener-Policy': 'same-origin',
+				'Cross-Origin-Embedder-Policy': 'require-corp'
+			}
 		}
 	}
 })
